26cfc834f748c11030d3d30fe85a191cb47943f4
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-04-29 15:43:02 +0200

    Add bonus

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,6 @@ const app = new Vue({
 
   mounted() {
     for (let i = 0; i < 10; i++) {
-        
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then(response => {
             //console.log(response);
