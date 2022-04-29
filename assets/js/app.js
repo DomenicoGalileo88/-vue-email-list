6f7f0e9ffe14c7c001de90272e5ce8c8c0b886e3
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-04-29 15:39:52 +0200

    Add mail list

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,20 @@
-const app = new Vue ({
-    el: '#app',
-    data:{},
+const app = new Vue({
+  el: "#app",
+  data: {
+    random_mails: []
+  },
 
-    methods:{},
-    
-    mounted(){
+  methods: {},
 
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+        
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then(response => {
+            //console.log(response);
+           //console.log(this);
+            this.random_mails.push(response.data.response);
+          });
     }
-})
+  },
+});
